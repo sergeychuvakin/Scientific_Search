6a887262aa79638673123a915286a8ea0e6df18c
Sergey <33733128+sergeychuvakin@users.noreply.github.com>
2019-07-07 19:51:58 +0300

    Add files via upload

---
+++ main/enhancment_words.py
@@ -0,0 +1,36 @@
+#Here are some lists of our topic keywords. We use them to help LSI determine the topic of the article
+
+cancerdict = ["tumor", "tumour", "trophoblastic", "triple-negative", "thyroid cancer", "thymoma", "sclc", "sarcoma",
+                  "rhabdomyosarcoma", "prostate", "pineoblastoma", "pheochromocytoma", "papillary", "paget's",
+                  "osteosarcoma", "oligodendroglioma", "nsclc", "non-small", "non-hodgkin", "neuroblastoma", "net",
+                  "myeloma", "myelodysplastic", "metastatic", "metastases", "mesothelioma", "mesenchymous",
+                  "meningioma", "melanoma", "medulloblastoma", "liposarcoma", "leukemia", "leiomyosarcoma",
+                  "infiltrating", "hodgkin's", "hodgkin", "hnc", "hemangioendothelioma", "glioma", "glioblastoma",
+                  "gastrointestinal", "fungoides", "fibrosarcoma", "ewing", "esophageal", "ependymoma",
+                  "craniopharyngioma", "colorectal", "chondrosarcoma", "chondrosarcoma", "cholangiocarcinoma",
+                  "cervical", "cancer", "breast", "astrocytoma", "angiosarcoma", "adenosarcoma", "adenocarcinoma"]      
+
+aizdict = ["aidp ", "aied ", "alps  ", "angiitis ", "antiphospholipid ", "autoimmune", "aps", "ar", "arteritis",
+               "buerger's ", "cah", "celiac", "cholangitis", "chronic", "churg-strauss", "cidp ", "cogan's ",
+               "cryoglobulinemia ", "cryopathies ", "css", "encephalomyelitis ", "gammopathy ", "gcv ",
+               "graft-versus-host", "granulomatosis", "gullain-barre", "gvhd ", "hypersensitivity ", "inflammatory",
+               "isaacs'", "kawasaki", "kd ", "lambert-eaton", "lems ", "lymphoproliferative", "lupus", "meniere's",
+               "mmn ", "moersch-woltmann ", "ms", "sclerosis", "myasthenia", "neuromyotonia", "oms",
+               "opsoclonus-myoclonus", "pan", "paraneoplastic", "pbc", "pernicious", "pnd", "pnd's", "polyarteritis",
+               "polyradiculoneuropathy", "psc", "rar", "raynaud's", "rheumatica ", "sarcoidosis ", "tak", "takayasu’s ",
+               "thromboangiitis", "vascolitides ", "vasculitis ", "wegener's"]
+infectdict = ["immunodeficiency", "bmd", "chb", "eculizumab", "hbeag", "hbsag", "hbv", "hcc", "hcv", "ifn",
+                  "infant", "liver", "rbv", "hiv", "hev", "hiv-1", "hiv-2", "hiv-3", "hiv+", "infection", "virus",
+                  "malaria", "hepatitis", "aids", "streptococcus", "antiretroviral", "art", "hiv-associated", "hand",
+                  "ebolavirus", "hiv-infected", "hiv-uninfected"]
+eyedict = ["amd", "cataract", "corneal", "eye", "glaucoma", "iol", "iop", "myopia", "poag", "retina", "retinal",
+               "retinopathy", "rgcs", "rnfl", "rop", "trachoma", "uveitis", "visual"]
+genedict = ["uremic", "ahus", "allele", "dmd", "dna", "dystrophin", "fviii", "gene", "genotype", "hemophilia",
+                "lncrna", "mdx", "mirna", "sma", "smn", "smn2", "snp", "tdf", "hereditary"]
+chemdict = ['bcl', 'bcl2', 'bcl-2', 'b-cell lymphoma', 'b-cell lymphoma 2', 'egfr', 'epidermal growth factor receptor',
+                'osimertinib', 't790m', 'c797s', 'cis', 'cytokine-induced stat inhibitor', 'socs', 'stat', 'cish', 'btk',
+                'bruton tyrosine kinase', 'ibrutinib', 'obinutuzumab', 'parp', "brca-2", "brca2", 'parp-1', 'poly adp ribose polymerase',
+                'olaparib', 'talazoparib', 'parpis', 'poly(adp-ribose) polymerase', 'poly(adp-ribose)', 'parpi', 'taar',
+                'trace amine-associated receptor', 'taar-5', 'taar-1', 'taar1', 'taar5', 'trace amine (ta)‐associated',
+                'cdk8', 'cdk19', 'cdks', 'cyclin-dependent protein kinases', 'cyclin-dependent protein kinase', 'retinoic-acid-related orphan receptor γt',
+                'rorγt', 'ror', 'ror gamma-t', 'ror(gamma)t', 'roryt', 'synthesis', 'compound', 'chemical']
